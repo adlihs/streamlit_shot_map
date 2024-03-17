3475f9c63e76891cf64142f9e8d10e62bd0c0fe4
Edmund Neil <edmondneil@gmail.com>
2024-03-17 16:14:05 -0300

    Add files via upload

--- pages/goal_secuence.py
+++ pages/goal_secuence.py
@@ -217,6 +217,13 @@ def viz_previous_events(soccer_data=None, game=None, minute=None):
 
 
 data = load_data()
+data = data[data['player'].notna()]
+mapeo = {
+    'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u','ü':'u', 'ø':'o',
+    'Á': 'A', 'É': 'E', 'Í': 'I', 'Ó': 'O', 'Ú': 'U','Ü':'U', 'Ø':'O'
+}
+# Reemplazar las letras con tilde por las mismas letras sin tilde
+data['player'] = data['player'].apply(lambda x: ''.join([mapeo.get(char, char) for char in x]))
 
 with st.sidebar:
     st.title('Pass Flow Generator :soccer:')
